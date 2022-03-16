7bbacb291fee225aacfabdbadde9d409a4daf894
hjm <hjm@hjmedu.com>
2022-03-16 16:46:18 +0800

    IdealVersion1.0

---
+++ src/main/java/com/hjmedu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hjmedu.git;
+
+/**
+ * @author 胡鑫
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello  git");
+    }
+}
